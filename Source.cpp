665ea439c425825cb7555bf4923922f2085470ad
lexamulat <lexamulat@mail.ru>
2018-03-23 23:55:34 +0300

    end

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include<cmath>
 
 using namespace std;
 
+
+void Show(vector<int> &mas) {
+	for (int i = 0; i < mas.size(); i++) {
+		cout << mas[i] << endl;
+	}
+}
+float AverageValue(vector<int> &mas) {
+	int count=0;
+	for (int i = 0; i < mas.size(); i++) {
+		count = count + mas[i];
+	}
+	float aver = count/mas.size();
+	return aver;
+}
 void shakeup(vector<int> &mas) {
 	int randomIndex;
 	int kol=rand() % (15 - 5 + 1) + 5;
@@ -19,38 +34,18 @@ void shakeup(vector<int> &mas) {
 
 }
 
-void menu(vector<int> &mas) {
-	int choise;
-	while (1) {
-		cout << endl<<"Action\n"
-			<< "(1)Show\n"
-			<< "(2)shakeup\n" << endl;
-
-		cin >> choise;
-		switch (choise) {
-		case 1: {
-			for (int i = 0; i < mas.size(); i++) {
-				cout << mas[i] << endl;
-			}
-			break;
-		}
-		case 2: {
-			shakeup(mas);
-			break;
-		}
-		case 7: {
-	
-			exit(0);
-		}
-		default: {
-			cout << "incorrect expression\n"
-				<< "try again\n";
-		}
-
-		}
+ void Multi(vector<int> &mas, vector<int> &copymas, vector<int> &result) {
+	for (int i = 0; i < mas.size(); i++) {
+		result.push_back(mas[i]*copymas[i]);
 	}
 }
 
+ void Square(vector<int> &mas) {
+	 for (int i = 0; i < mas.size(); i++) {
+		 mas[i] =pow(mas[i], 2);
+	 }
+ 
+ }
 int main() {
 	int size=5;
 	int m1 = 0, m2 = 15;
@@ -65,9 +60,23 @@ int main() {
 		int num = rand() % (m2 - m1 + 1) + m1;
 		mas.push_back(num);
 	}
-	menu(mas);
 
-
+	cout << endl << "makeCopy" << endl;
+	vector<int> copymas(mas);
+	cout << endl << "copymas:" << endl;
+	Show(copymas);
+	cout << endl << "Shake copymas:" << endl;
+	shakeup(copymas);
+	Show(copymas);
+	cout << endl << "average value" << endl;
+	cout << AverageValue(copymas);
+	cout << endl << "Multi" << endl;
+	vector<int> result;
+	Multi(mas, copymas, result);
+	Show(result);
+	cout << endl << "Squares" << endl;
+	Square(result);
+	Show(result);
 	system("pause");
 	return 0;
 }

cff1db4bb51b657c7c8360dbe6633a6d4297e926
lexamulat <lexamulat@mail.ru>
2018-03-23 22:48:10 +0300

    random shake

--- Source.cpp
+++ Source.cpp
@@ -4,24 +4,68 @@
 
 using namespace std;
 
+void shakeup(vector<int> &mas) {
+	int randomIndex;
+	int kol=rand() % (15 - 5 + 1) + 5;
+	//cout << "kol" << kol << endl;
+	int temp;
 
-void menu() {
+	for (int i = 0; i < kol - 1; i++) {
+		randomIndex = rand() % (mas.size() - 2);
+		temp = mas[randomIndex];
+		mas[randomIndex]=mas[randomIndex+1];
+		mas[randomIndex + 1] = temp;
+	}
 
+}
 
+void menu(vector<int> &mas) {
+	int choise;
+	while (1) {
+		cout << endl<<"Action\n"
+			<< "(1)Show\n"
+			<< "(2)shakeup\n" << endl;
+
+		cin >> choise;
+		switch (choise) {
+		case 1: {
+			for (int i = 0; i < mas.size(); i++) {
+				cout << mas[i] << endl;
+			}
+			break;
+		}
+		case 2: {
+			shakeup(mas);
+			break;
+		}
+		case 7: {
+	
+			exit(0);
+		}
+		default: {
+			cout << "incorrect expression\n"
+				<< "try again\n";
+		}
+
+		}
+	}
 }
 
 int main() {
 	int size=5;
+	int m1 = 0, m2 = 15;
 	/*cout << "enter vector's size" << endl;
 	cin >> size;*/
-	srand(time(NULL));
+	srand(time(0));
 
 	vector<int> mas;
+																								//а-лев b-пра
+																								//rand() % (b - a + 1) + a;
 	for (int i = 0; i < size; i++) {
-		int val = rand();
-		v.push_back(val % 20 - 10);
+		int num = rand() % (m2 - m1 + 1) + m1;
+		mas.push_back(num);
 	}
-
+	menu(mas);
 
 
 	system("pause");

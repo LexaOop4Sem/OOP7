2630d40addb93464061ded1166446505a6dfc8a4
lexamulat <lexamulat@mail.ru>
2018-03-24 18:01:38 +0300

    with alghoritm

--- Source.cpp
+++ Source.cpp
@@ -1,82 +1,81 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <ctime>
 #include<cmath>
+#include <numeric>
+#include <functional>
+#include <cstdlib> 
+
 
 using namespace std;
 
 
-void Show(vector<int> &mas) {
-	for (int i = 0; i < mas.size(); i++) {
-		cout << mas[i] << endl;
+
+void show(vector<int> &mas) {
+	cout << endl;
+	vector<int>::iterator iter;// creating int iterator
+	iter = mas.begin();
+	while (iter!=mas.end()) {
+		cout << *iter << " | ";
+		iter++;
 	}
+	cout << endl;
 }
+
+
 float AverageValue(vector<int> &mas) {
-	int count=0;
-	for (int i = 0; i < mas.size(); i++) {
-		count = count + mas[i];
-	}
-	float aver = count/mas.size();
+	int sum = accumulate(mas.begin(), mas.end(), 0);
+	float aver = (sum / mas.size());
 	return aver;
 }
-void shakeup(vector<int> &mas) {
-	int randomIndex;
-	int kol=rand() % (15 - 5 + 1) + 5;
-	//cout << "kol" << kol << endl;
-	int temp;
 
-	for (int i = 0; i < kol - 1; i++) {
-		randomIndex = rand() % (mas.size() - 2);
-		temp = mas[randomIndex];
-		mas[randomIndex]=mas[randomIndex+1];
-		mas[randomIndex + 1] = temp;
-	}
-
+//frequently call (make inline)
+ inline int random() { 
+	 {/*int m1 = 0, m2 = 15;
+	int num;
+	num = rand() % (m2 - m1 + 1) + m1;
+	return num;*/}
+	return (rand() % 14);
 }
 
- void Multi(vector<int> &mas, vector<int> &copymas, vector<int> &result) {
-	for (int i = 0; i < mas.size(); i++) {
-		result.push_back(mas[i]*copymas[i]);
-	}
-}
 
- void Square(vector<int> &mas) {
-	 for (int i = 0; i < mas.size(); i++) {
-		 mas[i] =pow(mas[i], 2);
-	 }
+ void Mysqr(vector<int> &mas, vector<int> &copymas) {
  
+	
+
  }
 int main() {
-	int size=5;
-	int m1 = 0, m2 = 15;
-	/*cout << "enter vector's size" << endl;
-	cin >> size;*/
-	srand(time(0));
+	srand(time(0)); //place important
+	size_t kol = 10;
+	vector<int> mas(kol);
+	generate(mas.begin(), mas.end(), random);
+	cout << endl << "   MAS    " << endl;
 
-	vector<int> mas;
-																								//а-лев b-пра
-																								//rand() % (b - a + 1) + a;
-	for (int i = 0; i < size; i++) {
-		int num = rand() % (m2 - m1 + 1) + m1;
-		mas.push_back(num);
-	}
-
-	cout << endl << "makeCopy" << endl;
+	show(mas);
+	cout<< endl << "Creating copy of mas    COPYMAS:  "<<endl;
+	
 	vector<int> copymas(mas);
-	cout << endl << "copymas:" << endl;
-	Show(copymas);
-	cout << endl << "Shake copymas:" << endl;
-	shakeup(copymas);
-	Show(copymas);
-	cout << endl << "average value" << endl;
+	show(copymas);
+	cout << endl << "shake it: " << endl;
+
+	random_shuffle(copymas.begin(), copymas.end());
+	show(copymas);
+
+	cout << endl << "Count average" << endl;
 	cout << AverageValue(copymas);
-	cout << endl << "Multi" << endl;
-	vector<int> result;
-	Multi(mas, copymas, result);
-	Show(result);
-	cout << endl << "Squares" << endl;
-	Square(result);
-	Show(result);
+	cout << endl << "Multiply vectors  (MAS)*(COPYMAS) : " << endl;
+	vector<int> result(kol);
+	transform(mas.begin(), mas.end(), copymas.begin(), result.begin(),
+		multiplies <int>());
+	show(result);
+	cout << endl << "Sqr result : " << endl;
+	transform(result.begin(), result.end(), result.begin(),result.begin(),
+		multiplies <int>());
+	show(result);
+
 	system("pause");
 	return 0;
 }
+
+
